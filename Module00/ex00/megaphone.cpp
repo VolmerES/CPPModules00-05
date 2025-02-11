8b30da410b6eb410687e2d5b1940858f8113d6ed
Jdelorme <volmer394@gmail.com>
2025-02-11 12:40:49 +0100

    fix: sometimes doesn't print spaces

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 16:34:11 by jdelorme          #+#    #+#             */
-/*   Updated: 2025/02/10 16:03:47 by volmer           ###   ########.fr       */
+/*   Updated: 2025/02/11 12:35:33 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,27 @@
 #include <ctype.h>
 
 
-int	main(int argc, char **argv)
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
 {
-	int	i (0);
-	int j (0);
-	int flag;
-	
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	while (argv[++i])
-	{
-		j = -1;
-		while (argv[i][++j])
-		{
-			flag = 0;
-			if (argv[i][j] == ' ' && flag == 0)
-			{
-				std::cout << " ";
-				flag = 1;
-			}
-			if (argv[i][j] != ' ')
-			{
-				std::cout << (char)toupper(argv[i][j] + 0);
-			}
-		}
-		
-	}
-	std::cout << std::endl;
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; argv[i][j]; j++)
+        {
+            std::cout << (char)std::toupper(argv[i][j]);
+        }
+        if (i < argc - 1)
+            std::cout << " ";
+    }
+    
+    std::cout << std::endl;
+    return 0;
 }

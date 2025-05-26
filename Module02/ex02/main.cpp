f7d410d4f7d8d9e58be657ab47a7bd184f367f12
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-05-26 19:24:12 +0200

    add: subject main

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 20:37:34 by volmer            #+#    #+#             */
-/*   Updated: 2025/05/22 21:44:39 by volmer           ###   ########.fr       */
+/*   Updated: 2025/05/26 19:20:43 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,17 @@
 int	main(void)
 {
 	Fixed		a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d ( b );
+	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-	a =  Fixed( 1234.4321f );
-
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 	
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << b << std::endl;
+	
+	std::cout << Fixed::max( a,b ) << std:endl;
+
+	return(0);
 }

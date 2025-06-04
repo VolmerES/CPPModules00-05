dbb23a7d2fcfba4bafa35827b051cfc90776040d
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-06-04 21:39:28 +0200

    add brain class to dog/cat canonical constructors

--- Module04/ex01/src/cat.cpp
+++ Module04/ex01/src/cat.cpp
@@ -6,7 +6,7 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 19:25:58 by volmer            #+#    #+#             */
-/*   Updated: 2025/06/04 19:38:56 by volmer           ###   ########.fr       */
+/*   Updated: 2025/06/04 21:39:00 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 Cat::Cat(void) {
 	std::cout << "Cat constructor called" << std::endl;
+	this->_brain = new Brain();
 	this->_type = "Cat";
 }
 
 Cat::Cat(const Cat& other) {
+	this->_brain = new Brain(*other._brain);
 	this->_type = other._type;
 }
 
 Cat& Cat::operator=(const Cat& other) {
 	if (this != &other) {
+		delete this->_brain;
+		this->_brain = new Brain(*other._brain);
 		this->_type = other._type;
 	}
 	return (*this);
@@ -30,6 +34,7 @@ Cat& Cat::operator=(const Cat& other) {
 
 Cat::~Cat(void) {
 	std::cout << "Cat destructor called" << std::endl;
+	delete this->_brain;
 }
 
 void	Cat::makeSound(void) const {
--- Module04/ex01/src/dog.cpp
+++ Module04/ex01/src/dog.cpp
@@ -6,7 +6,7 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 19:35:26 by volmer            #+#    #+#             */
-/*   Updated: 2025/06/04 19:38:27 by volmer           ###   ########.fr       */
+/*   Updated: 2025/06/04 21:34:24 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,20 @@
 
 Dog::Dog(void) {
 	std::cout << "Dog constructor called" << std::endl;
+	this->_brain = new Brain();
 	this->_type = "Dog";
 }
 
 Dog::Dog(const Dog& other) {
+	
+	this->_brain = new Brain(*other._brain);
 	this->_type = other._type;
 }
 
 Dog& Dog::operator=(const Dog& other) {
 	if (this != &other) {
+		delete this->_brain;
+		this->_brain = new Brain(*other._brain);
 		this->_type = other._type;
 	}
 	return (*this);
@@ -30,6 +35,7 @@ Dog& Dog::operator=(const Dog& other) {
 
 Dog::~Dog(void) {
 	std::cout << "Dog destructor called" << std::endl;
+	delete this->_brain;
 }
 
 void	Dog::makeSound(void) const {
